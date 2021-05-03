finished arange. First test of derivatives (some fixes)

parallelization/tools/plot.py
@@ -3,8 +3,17 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 if __name__ == "__main__":
-    data = pd.read_csv("out/test.csv")
-    data = np.array(data)
+    # data = pd.read_csv("out/foo.csv", sep=",")
+    # data = np.array(data).transpose()
+    # plt.plot(data[0], data[1], label="f(x)")
 
-    plt.plot(data[0], data[1])
+    data = pd.read_csv("out/foo_dd_a.csv")
+    data = np.array(data).transpose()
+    plt.plot(data[0], data[1], label="f''(x) a")
+
+    data = pd.read_csv("out/foo_dd_n.csv")
+    data = np.array(data).transpose()
+    plt.plot(data[0], data[1], label="f''(x) n")
+
+    plt.legend(loc="best")
     plt.show()